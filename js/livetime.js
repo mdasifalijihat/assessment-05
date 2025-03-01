c595e12eda8b6051e04253bdd07a5570d929db48
mdasifalijihat <mdasifalijihat@gmail.com>
2025-03-01 22:45:41 +0600

    add

--- js/livetime.js
+++ js/livetime.js
@@ -12,5 +12,5 @@ function updateDayAndDate() {
 
     // discover blogs
     document.getElementById("discover-blog").addEventListener("click", function () {
-        window.location.href = "/blog.html";
+        window.location.href = "/assessment-05/blog.html";
       });
